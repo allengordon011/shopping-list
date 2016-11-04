0eb0e1707f3f947afb07468a6b02fc13721da390
trig0 <allengordon0@gmail.com>
2016-11-04 11:17:28 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-//1)set up state (with links to .js and jQuery)
-//2)set up event listener for user input
-//3)cross reference user input with state
-//4)output 'new' state
+//*set up state (with links to .js and jQuery) - state object
+//user wants to add an item
+//user needs a text input box and a submit button
+//*set up event listener for user input - event listener
+//user wants to see their items in a list
+//*cross reference event listener user input with state - state modification functions
+//*each new item should be appended, *or pre-pended* - state modification functions
+//*output 'new' state - state rendering function
+
+//requirements: 
+// -enter items they need to purchase by entering text and 
+// hitting "Return" or clicking the "Add item" button
+// -check and uncheck items on the list by clicking the "Check" button
+// -permanently remove items from the list
+
+$(document).ready(function()){
+	//state object
+	var state = {
+		items: []
+	}
+	//event listeners
+	$('.shopping-list-add-button').on('click', function(event){
+		event.preventDefault();
+		var userInput = (this.event);
+	});
+	//state modification functions
+	var addItem = function (state, userInput) {
+		state.items.push(userInput);
+	}
+	//state rendering functions
+	$('.shopping-list').prepend(new_item);
+
+	('<div class="shopping item>' + userInput + '</div>')
+
+})
+
+//nothing here
