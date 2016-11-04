7cbd219d5616197de6cee2575463e687f3fd0b93
lucas <lucas@lucass-MacBook-Pro-2.local>
2016-11-04 14:57:19 -0400

    updated app.js for shopping list application

--- app.js
+++ app.js
@@ -13,9 +13,10 @@
 // -check and uncheck items on the list by clicking the "Check" button
 // -permanently remove items from the list
 
-$(document).ready(function()){
+$(document).ready(function(){
 
 	//state object
+	//an array of "item: userInput" objects with property "checked: true/false"
 	var state = {
 		items: [
 			{ title: "apples",
@@ -23,28 +24,79 @@ $(document).ready(function()){
 			{ title: "bananas",
 			checked: false }
 		]
+		// console.log("state object " + state.items)
 	}
 
 	//event listeners
+	/*
+	- needs: user input
+	- grabs text from input field when "submit" button is clicked
+	- passes info to addItem
+	- passes info to updateList
+	- >> form resets
+	*/
 	$('.shopping-list-add-button').on('click', function(event){
 		event.preventDefault();
-		//var userInput = (this.event);
-		addItem(??, ??);
-		updateList(??, $('.shopping-list'));
+		//
+		var userInput = ($('.shopping-list-add-input').val());
+
+		addItem(state, userInput);
+		updateHtml(state, $('.shopping-list'));
+
+		console.log("event listeners " + userInput);
+
 	});
 
 	//state modification functions
+	/*
+	- needs: state, userInput
+	- creates new object using user input
+	- pushes object to state object array
+	*/
 	var addItem = function (state, userInput) {
-		//userInput is just text
-		var newObject = {title: userInput, checked: false}
+		var newObject = {
+			title: userInput,
+			checked: false }
 		state.items.push(newObject);
+
+		console.log("state modification " + newObject);
+
 	}
 
 	//state rendering functions
-	var updateList = function (state, element) {
-		$('.shopping-list').prepend('<div class="shopping-item">' + $(newObject[-1]) + '</div>');
-	}
+	/*
+	- needs: state, element to update
+	- uses jQuery to add html/css to element (iterates over all items)
+	- replaces old HTML element with updated HTML element
+	*/
+	//first parameter is the state, element to render into is the second
+	var updateHtml = function (state, element) {
+		// element = $('.shopping-list')
+		var itemsHtml = state.items.map(function(item) {
+			return '<li>' + item + '</li>';
+		});
+		
+		element.html(itemsHtml);
 
-	updateList(state, 'testItem');
+		console.log("state rendering functions " + itemsHtml);
+
+	};
+
+});
 
 //nothing here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
