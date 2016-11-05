02bdffc0fbd7afc3930d9b062f09a81a09accfa4
Hannah <hannahjmccain@gmail.com>
2016-11-05 15:08:58 -0400

    half-finished shopping list app

--- app2.js
+++ app2.js
@@ -2,25 +2,26 @@ $(document).ready(function() {
 
 //state
 var state = {
-	items: [
-    { title: "peaches", checked: false },
-    { title: "honey", checked: false },
-	]
+	items: []
 }
 
 // manipulate
-// state.items as argument
+var addItem = function (state, userInput) {
+	state.items.push( { title: userInput, checked: false })
+}
+
+// iterate through state.items to assign ID to shopping list objects
 var assignId = function(arrayOfObjects) {
   arrayOfObjects.map(function (object, index) {
     object.itemID = (index + 1); 
   });
   var state = arrayOfObjects;
-  return state;
+  // return state;
 }
 
 // render
 var renderHtml = function (state, element) {
-// state.items as argument
+// iterate through state.items to get individual object attributes
 	var findObjectContents = function(arrayOfObjects) {
 		var checkButton = '<button class="check-button">Check</button>';
 		var deleteButton = '<button class="delete-button">Delete</button>';
@@ -41,10 +42,45 @@ var renderHtml = function (state, element) {
 
 assignId(state.items);
 findObjectContents(state.items);
+element.html(findObjectContents);
 
 }
 
 // listen
-renderHtml(state, $('.shopping-list'));
+var newItemText = $('.shopping-list-add').submit(function(event) {
+	event.preventDefault();
+	var userInput = $('.shopping-list-add-input').val();
+
+	// pass out commands for next steps
+	addItem(state, userInput);
+	renderHtml(state, $('.shopping-list'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
